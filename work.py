5604385eec520a269bf9d14a37f77d42f0986348
Akshit051604 <akshitbaren0516@gmail.com>
2024-09-18 01:12:37 +0530

    Update and rename work to work.py

--- work.py
+++ work.py
@@ -32,13 +32,18 @@ def quit_app():
 # Create the GUI window
 root = tk.Tk()
 root.title("LED Control")
-root.title("Lights")
+heading = tk.Label(root, text="Lights", font=("Helvetica", 24))
+heading.pack()
 
 # Create radio buttons
 led_var = tk.IntVar()  # Use IntVar for integer values
 red_button = tk.Radiobutton(root, text="Red", value=1, variable=led_var, command=led_select)
 white_button = tk.Radiobutton(root, text="White", value=2, variable=led_var, command=led_select)
 green_button = tk.Radiobutton(root, text="Green", value=3, variable=led_var, command=led_select)
+
+#Reset Button
+reset_button = tk.Button(root, text="Reset", command= reset_led)
+reset_button.pack()
 
 # Create exit button
 exit_button = tk.Button(root, text="Exit", command=quit_app)
